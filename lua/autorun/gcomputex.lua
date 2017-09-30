2aa0a30b12245f24faf5457fd15b1c8a8225ba94
swadical <swadical@lab.glua.team>
2017-09-30 21:22:26 +0930

    load galileo during autorun

--- lua/autorun/gcomputex.lua
+++ lua/autorun/gcomputex.lua
@@ -1,3 +1,8 @@
+local __rtl = include("__rtl.lua")
+debug.getregistry().__rtl = __rtl
+
+galileo = __rtl.package.require ("../galileo/lua/init")
+
 if SERVER or
    file.Exists ("gcomputex/gcompute.lua", "LUA") or
    file.Exists ("gcomputex/gcompute.lua", "LCL") and GetConVar ("sv_allowcslua"):GetBool () then
